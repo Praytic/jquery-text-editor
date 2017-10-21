dfbe58def11bad5d07d571408a3a04e489bf4e98
vchernogorov <vchernogorov@griddynamics.com>
2017-10-21 23:25:01 +0400

    Save

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 $('.toolbar a').click(function(e) {
   var command = $(this).data('command');
   if (command == 'insertimage') {
-    url = prompt('Enter the link here: ', 'http:\/\/');
+    url = prompt('Enter the link here: ', '');
     document.execCommand($(this).data('command'), false, url);
   } else document.execCommand($(this).data('command'), false, null);
 });
