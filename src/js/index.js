2b64db85a190f0288ad124eb0bb4cd5bd9753b99
vchernogorov <vchernogorov@griddynamics.com>
2017-11-11 18:40:07 +0400

    Implement table insertion into editor

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,33 @@
-$('.toolbar a').click(function(e) {
-  var command = $(this).data('command');
-  if (command == 'insertimage') {
-    url = prompt('Enter the link here: ', '');
-    document.execCommand($(this).data('command'), false, url);
-  } else document.execCommand($(this).data('command'), false, null);
-});
+(function ($) {
+  $('.toolbar a').click(function (e) {
+    var command = $(this).data('command');
+    if (command === 'insertimage') {
+      url = prompt('Enter the link here: ', '');
+      document.execCommand(command, false, url);
+    }
+    else if (command === 'insertTable') {
+      const rowsNumber = $('#rows-number').val();
+      const colsNumber = $('#cols-number').val();
+      var tableStr = "<table class='custom-table'>";
+      tableStr += "<thead><tr>";
+      for (var i = 0; i < colsNumber; i++) {
+        tableStr += "<th></th>";
+      }
+      tableStr += "</thead><tbody>";
+      for (var i = 0; i < rowsNumber; i++) {
+        tableStr += "<tr>";
+        for (var j = 0; j < colsNumber; j++) {
+          tableStr += "<td></td>"
+        }
+        tableStr += "</tr>";
+      }
+      tableStr += "</tbody>";
+      tableStr += "</table>";
+
+      document.execCommand("insertHTML", false, tableStr);
+    }
+    else {
+      document.execCommand(command, false, null);
+    }
+  });
+})(jQuery);
